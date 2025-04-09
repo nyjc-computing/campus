b7fa96467d2f9fc99712f56bf2d2e6c2eb46e4d7
nyjc.computing <164971954+nycomp@users.noreply.github.com>
2025-04-09 13:46:18 +0000

    [feat] implement GET endpoints

--- apps/palmtree/routes/clients/__init__.py
+++ apps/palmtree/routes/clients/__init__.py
@@ -1,9 +1,13 @@
-from flask import Blueprint
+from flask import Blueprint, request
 
 from apps.palmtree.models import client
+from common.schema import Message
 
 bp = Blueprint('clients', __name__, url_prefix='/clients')
 
+client_requests = client.ClientIdRequest()
+clients = client.Client()
+api_keys = client.ClientAPIKey()
 
 
 def init_app(app) -> None:
@@ -15,30 +19,45 @@ def init_app(app) -> None:
 @bp.post('/')
 def apply_for_client():
     """Apply for a client id and secret."""
-    return {"message": "not implemented"}, 501
+    # TODO: validate request
+    data = request.get_json()
+    resp = client_requests.submit_client_request(**data)
+    match resp:
+        case ("error", msg, _):
+            return {"error": msg}, 500
+        case ("ok", Message.CREATED, _):
+            return {"message": "Client request submitted"}, 201
+    return {"message": "unexpected error occurred"}, 500
 
 
 @bp.patch('/')
 def edit_client():
     """Edit name, description, or admins of client."""
-    # TODO: admin required
+    # TODO: validate request, authenticate
     return {"message": "not implemented"}, 501
 
 
-@bp.get('/applications/<string:application_id>')
-def get_application_status(application_id: str):
+@bp.get('/applications/<string:requester_id>')
+def get_application_status(requester_id: str):
     """Get the status of a client application."""
-    return {"message": "not implemented"}, 501
+    # TODO: validate, authenticate
+    resp = client_requests.get_client_request(requester_id)
+    match resp:
+        case ("error", msg, _):
+            return {"error": msg}, 500
+        case ("ok", Message.FOUND, result):
+            return result, 200
+    return {"message": "unexpected error occurred"}, 500
 
-
-@bp.post('/applications/<string:application_id>/approve')
-def approve_application(application_id: str):
+@bp.post('/applications/<string:requester_id>/approve')
+def approve_application(requester_id: str):
     """Approve a client application."""
+    # TODO: validate, authenticate
     return {"message": "not implemented"}, 501
 
 
 @bp.post('/applications/<string:application_id>/reject')
-def reject_application(application_id: str):
+def reject_application(requester_id: str):
     """Reject a client application."""
     return {"message": "not implemented"}, 501
 
@@ -46,7 +65,14 @@ def reject_application(application_id: str):
 @bp.get('/<string:client_id>')
 def get_client_details(client_id: str):
     """Get details of a client."""
-    return {"message": "not implemented"}, 501
+    # TODO: validate, authenticate
+    resp = clients.get_client(client_id)
+    match resp:
+        case ("error", msg, _):
+            return {"error": msg}, 500
+        case ("ok", Message.FOUND, result):
+            return result, 200
+    return {"message": "unexpected error occurred"}, 500
 
 
 @bp.post('/<string:client_id>/revoke')
@@ -58,7 +84,14 @@ def revoke_client(client_id: str):
 @bp.get('/<string:client_id>/api_keys/')
 def get_client_api_keys(client_id: str):
     """Get API keys requested by client admin."""
-    return {"message": "not implemented"}, 501
+    # TODO: validate, authenticate
+    resp = api_keys.get_api_keys(client_id)
+    match resp:
+        case ("error", msg, _):
+            return {"error": msg}, 500
+        case ("ok", Message.FOUND, result):
+            return result, 200
+    return {"message": "unexpected error occurred"}, 500
 
 
 @bp.post('/<string:client_id>/api_keys/create')
