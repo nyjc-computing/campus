9a87fef63cc702625adcb042fceaa9365a07a194
ngjunsiang <ngjunsiang@gmail.com>
2025-05-01 16:29:24 +0000

    [refactor] Move error handler registration to errors.py

--- apps/palmtree/__init__.py
+++ apps/palmtree/__init__.py
@@ -11,15 +11,10 @@ from .routes import emailotp
 from .routes import users
 
 
-def api_internal_server_error(err):
-    # TODO: Error logging
-    return {"message": "Internal server error"}, 500
-
 def create_app() -> Flask:
     """Factory function to create the Palmtree app."""
     # Organise API routes under api blueprint
     bp = Blueprint('api_v1', __name__, url_prefix='/api/v1')
-    bp.register_error_handler(Exception, api_internal_server_error)
     # Users need to be initialised first as other blueprints
     # rely on user table
     users.init_app(bp)
