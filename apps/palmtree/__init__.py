18d5022a7e8a3ce70e6ea53778084848ff734da4
JS Ng <ngjunsiang@gmail.com>
2025-05-10 13:48:47 +0000

    [feat] Enable both init_app and create_app patterns

--- apps/palmtree/__init__.py
+++ apps/palmtree/__init__.py
@@ -5,14 +5,24 @@ Authentication and authorization service for Campus.
 
 from flask import Blueprint, Flask
 
-from . import errors
+from apps.common import errors
 from .routes import clients
 from .routes import emailotp
 from .routes import users
 
 
 def create_app() -> Flask:
-    """Factory function to create the Palmtree app."""
+    """Factory function to create the Palmtree app.
+    
+    This is called if Palmtree is run as a standalone app.
+    """
+    app = Flask(__name__)
+    init_app(app)
+    errors.init_app(app)
+    return app
+
+def init_app(app: Flask) -> None:
+    """Initialise the Palmtree API blueprint with the given Flask app."""
     # Organise API routes under api blueprint
     bp = Blueprint('api_v1', __name__, url_prefix='/api/v1')
     # Users need to be initialised first as other blueprints
@@ -20,8 +30,4 @@ def create_app() -> Flask:
     users.init_app(bp)
     clients.init_app(bp)
     emailotp.init_app(bp)
-
-    app = Flask(__name__)
     app.register_blueprint(bp)
-    errors.init_app(app)
-    return app
