9e6c1fa386db967242070ac44b1194c693d691ce
JS Ng <ngjunsiang@gmail.com>
2025-05-11 16:07:31 +0000

    [fix] make routes module importable

---
+++ apps/palmtree/routes/__init__.py
@@ -0,0 +1,12 @@
+"""apps/palmtree/routes
+
+This is a namespace module for the Palmtree API routes.
+"""
+
+from . import clients, emailotp, users
+
+__all__ = [
+    "clients",
+    "emailotp",
+    "users",
+]
