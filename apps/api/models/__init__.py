6032b446aa208823b9027aeb210207cd63397ddc
JS Ng <ngjunsiang@gmail.com>
2025-05-17 16:07:13 +0000

    docs: update Campus API Model
    This commit adds typing and naming conventions.

--- apps/api/models/__init__.py
+++ apps/api/models/__init__.py
@@ -30,6 +30,14 @@ resources.
 - It is worth leaving some performance on the table to keep the data model
   flexible.
 
+### Avoid tight coupling
+  
+- Avoid tight coupling with frameworks.
+- where it is easy to do so, use callbacks and hooks that allow behaviour to be
+  specified or customised by users, unless this overly obscures the model logic.
+- As much as possible, promote the use of decorators and other Pythonic patterns
+  to allow users to extend the model behaviour.
+
 ## Conventions
 
 ### Consistent ID pattern
@@ -46,11 +54,33 @@ resources.
 - Model users should be able to intuit the method arguments, rquiring
   documentation only to confirm usage or understand nuances more clearly.
 
-### Avoid tight coupling
-  
-- Avoid tight coupling with frameworks.
-- where it is easy to do so, use callbacks and hooks that allow behaviour to be
-  specified or customised by users, unless this overly obscures the model logic.
-- As much as possible, promote the use of decorators and other Pythonic patterns
-  to allow users to extend the model behaviour.
+### Support native Python types
+
+- Campus is not trying to be a macroframework; a user should not need to know
+  an entire class hierarchy to implement a Campus server.
+- Where annotation and encapsulation is desired, try to subclass or extend the
+  behaviour of existing Python classes and APIs, in a way that does not obscure
+  the logic of the model.
+- However, low-level details should be delegated to util functions, for
+  easier standardised handling of common tasks, e.g. datetime conversion and
+  formatting.
+
+### Typing conventions
+
+- Request and response bodies should be typed using TypedDict.
+- Where TypedDicts are an acceptable argument type, use Mapping[str, Any] or
+  Mapping[str, JsonSerializable].
+- Explicitly specify total=True or total=False for TypedDicts.
+- Use NotRequired for record/resource fields that might not exist, or be None.
+- When the project is updated to python>=3.13, use ReadOnly to indicate fields
+  that should not be modified, e.g. `id`, `created_at`.
+
+### Naming conventions
+
+- Request and response body schemas should be suffixed with the verb they
+  represent, e.g. `UserNew`, `UserUpdate`.
+- Model schemas should be suffixed with `Resource`, e.g. `UserResource`.
+- Models and schemas should be named in singular, e.g. `UserResource`, since
+  they represent a single resource record.  
+  This makes it easier to differentiate records and resources.
 """
