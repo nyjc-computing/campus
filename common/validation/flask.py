6083747a768122d0f13c5b5c509e543bbcca3da9
JS Ng <ngjunsiang@gmail.com>
2025-05-17 16:58:57 +0000

    feat: extend validation to responses

--- common/validation/flask.py
+++ common/validation/flask.py
@@ -1,20 +1,21 @@
-"""common/validation/request
+"""common/validation/flask
 
-Common utility functions for validation of request body.
+Common utility functions for validation of flask requests and responses.
 """
 
-from functools import partial, wraps
+from functools import wraps
 from json import JSONDecodeError
 from typing import Any, Callable, Mapping, Protocol, Type
 
-from flask import Request, request
+from flask import Request, request as flask_request
 
 from common.validation import record
 
 # Only expecting strings or dicts
 JsonSerializable = str | dict[str, Any]
 StatusCode = int
-# Actually, view functions may return a variety of return values which Flask is able to handle
+# Actually, view functions may return a variety of return values which Flask is
+# able to handle
 # But Campus API sticks to JSON-serializable return values, with a status code
 FlaskResponse = tuple[JsonSerializable, StatusCode]
 ErrorHandler = Callable[[StatusCode], None]
@@ -36,7 +37,7 @@ class ValidatedViewFunction(Protocol):
     unpacking its contents into an inner view function.
     """
 
-    def __call__(self, *args: str) -> partial[FlaskResponse]:
+    def __call__(self, *args: str) -> FlaskResponse:
         """A validated view function takes no keyword arguments, only
         positional arguments.
         Positional arguments are expected to come from the request path, e.g.
@@ -46,16 +47,16 @@ class ValidatedViewFunction(Protocol):
         ...
 
 
-def unpack_json(request_: Request, on_error: ErrorHandler) -> JsonSerializable:
+def unpack_json(request: Request, on_error: ErrorHandler) -> JsonSerializable:
     """Unpacks JSON body from the request.
     Calls the given error handler if unable to do so.
     """
     # Mimetype check
-    if not request_.is_json:
+    if not request.is_json:
         on_error(415)
     # Unpack request JSON body
     try:
-        payload = request_.get_json()
+        payload = request.get_json()
     except JSONDecodeError:
         on_error(400)
     except Exception:
@@ -64,8 +65,10 @@ def unpack_json(request_: Request, on_error: ErrorHandler) -> JsonSerializable:
         return payload
     raise AssertionError("Unreachable code")  # pragma: no cover
 
-def validate_request(
-        schema: Mapping[str, Type],
+def validate_schema(
+        *,
+        request: Mapping[str, Type] | None = None,
+        response: Mapping[str, Type] | None = None,
         on_error: ErrorHandler = lambda s: None,
 ) -> Callable[[ViewFunction], ValidatedViewFunction]:
     """Returns a decorator that takes a view-function and returns a
@@ -75,27 +78,44 @@ def validate_request(
     def vfdecorator(vf: ViewFunction) -> ValidatedViewFunction:
         """Validates the current Flask request JSON body, and unpacks it into
         the wrapped view-function.
+        Validates the 
         """
         # TODO: provide helpful validation hints
-        payload = unpack_json(request, on_error)
+        payload = unpack_json(flask_request, on_error)
         # Validate request body
         assert isinstance(payload, dict), "Request body must be a JSON object"
-        try:
-            record.validate_keys(
-                payload,
-                schema,
-                ignore_extra=True,
-                required=True,
-            )
-        except (KeyError, TypeError):
-            on_error(400)
-        except Exception:
-            on_error(500)
+        if request is not None:
+            try:
+                record.validate_keys(
+                    payload,
+                    request,
+                    ignore_extra=True,
+                    required=True,
+                )
+            except (KeyError, TypeError):
+                on_error(400)
+            except Exception:
+                on_error(500)
 
         @wraps(vf)
-        def validatedvf(*args: str) -> partial[FlaskResponse]:
+        def validatedvf(*args: str) -> FlaskResponse:
             """The decorated ValidatedViewFunction that unpacks the response JSON body into the inner view-function."""
-            return partial(vf, **payload)
+            resp_json, status_code = vf(*args, **payload)
+            assert isinstance(resp_json, dict), "Response body must be a JSON object"
+            if response is not None and 200 <= status_code < 300:
+                try:
+                    record.validate_keys(
+                        resp_json,
+                        response,
+                        ignore_extra=True,
+                        required=True,
+                    )
+                except (KeyError, TypeError):
+                    on_error(500)
+                except Exception:
+                    on_error(500)
+            return resp_json, status_code
+                
 
         return validatedvf
     return vfdecorator
