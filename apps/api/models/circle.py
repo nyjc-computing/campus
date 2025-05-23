abade8c23b3313243746001d3468619e461da9ab
ngjunsiang <ngjunsiang@gmail.com>
2025-05-23 23:40:51 +0800

    feat: implement address tree

--- apps/api/models/circle.py
+++ apps/api/models/circle.py
@@ -2,19 +2,39 @@
 Circle Models
 
 This module provides classes for managing Campus circles.
+
+Data structures:
+- collections (Circle)
+- address tree
+
+Main operations:
+- CRUD
+- resolve source access for a particular user
+- get flat list of users (leaf circles) of any circle
+- move circles
 """
 
 from typing import NotRequired, TypedDict, Unpack
 
 from apps.common.errors import api_errors
 from apps.api.models.base import BaseRecord, ModelResponse
-from common.drum.mongodb import get_drum
+from common.drum.mongodb import PK, get_conn, get_drum
 from common.schema import CampusID, UserID, Message, Response
 from common.utils import uid, utc_time
 
+# TODO: Replace with OpenAPI-based string-pattern schema
+AccessValue = int
+CircleID = CampusID
+CirclePath = str
+CircleTag = str
+CircleTree = dict[CircleID, "CircleTree"]
+
+# TODO: Make domain configurable
+DOMAIN = "nyjc.edu.sg"
 TABLE = "circles"
 
 
+# TODO: Refactor settings into a separate model
 def init_db():
     """Initialize the collections needed by the model.
 
@@ -22,15 +42,67 @@ def init_db():
     staging.
     For MongoDB, collections are created automatically on first insert.
     """
-    # No-op for MongoDB, but you could create indexes here if needed.
+    # Create admin and root circles
+    root_circle = Circle().new(
+        name=DOMAIN,
+        description="Root circle",
+        tag="root",
+        parents={}
+    ).data
+    admin_circle = Circle().new(
+        name="campus-admin",
+        description="Campus admin account",
+        tag="admin",
+        parents={root_circle[PK]: 15}
+    ).data
+
+    storage = get_conn()
+    storage["settings"].update(
+        {"category": "admin"},
+        {"$set": {"circle_id": admin_circle[PK]}},
+        upsert=True
+    )
+    storage["settings"].update(
+        {"category": "circles"},
+        {"$set": {"root": root_circle[PK]}},
+        upsert=True
+    )
+
+    # Create circle address tree
+    storage[TABLE].insert_one({root_circle[PK]: {}})
+
+
+def get_root_circle() -> "CircleRecord":
+    """Get the root circle ID from the settings collection."""
+    storage = get_conn()
+    circle_settings = storage["settings"].find_one({"category": "circles"})
+    if circle_settings is None:
+        raise api_errors.InternalError(
+            message="Root circle not found in settings",
+            id=DOMAIN
+        )
+    return Circle().get(circle_settings["root"]).data
+
+
+def get_tree_root() -> CircleTree:
+    """Get the root of the Circle tree"""
+    root = get_root_circle()
+    return get_conn()[TABLE].find_one(
+        {root[PK]: {"$exists": True}}
+    )
 
 
 class CircleNew(TypedDict, total=True):
-    """Request body schema for a circles.new operation."""
+    """Request body schema for a circles.new operation.
+
+    Circles must be created with at least one parent (default: admin).
+    The `parents` property maps the circle's full path
+      ({parent path} / {circle_id}) to its access value in that parent.
+    """
     name: str
     description: NotRequired[str]
-    tag: str
-    parents: list[]
+    tag: CircleTag
+    parents: dict[CirclePath, AccessValue]
 
 
 class CircleUpdate(TypedDict, total=False):
@@ -40,12 +112,14 @@ class CircleUpdate(TypedDict, total=False):
     # tag cannot be updated once created
 
 
-class CircleRecord(CircleNew, BaseRecord, TypedDict, total=True):
+class CircleRecord(CircleNew, BaseRecord):
     """The circle record stored in the circle collection."""
 
 
-class CircleResource(CircleRecord, TypedDict, total=True):
+class CircleResource(CircleRecord, total=False):
     """Response body schema representing the result of a circles.get operation."""
+    children: dict[CircleID, AccessValue]
+    sources: dict  # SourceID, SourceHeader
 
 
 class Circle:
@@ -61,7 +135,12 @@ class Circle:
         It does not add it to the circle hierarchy or access control.
         """
         circle_id = uid.generate_category_uid("circle", length=8)
-        record = CircleResource(
+        if len(fields["parents"]) == 0:
+            raise api_errors.InvalidRequestError(
+                message="Circle must have at least one parent",
+                id=circle_id,
+            )
+        record = CircleRecord(
             id=circle_id,
             created_at=utc_time.now(),
             **fields,
@@ -96,6 +175,7 @@ class Circle:
     def get(self, circle_id: str) -> ModelResponse:
         """Get a circle by id from the circle collection."""
         resp = self.storage.get_by_id(TABLE, circle_id)
+        # TODO: join with sources and access values
         match resp:
             case Response(status="error", message=message, data=error):
                 raise api_errors.InternalError(message=message, error=error)
@@ -122,3 +202,25 @@ class Circle:
                     id=circle_id
                 )
         raise ValueError(f"Unexpected response from storage: {resp}")
+
+
+class CircleAddressTree:
+    """Circle address tree for managing circle hierarchy.
+
+    While each circle already stores its descendancy information,
+    this class provides a more efficient way to trace members of any circle.
+
+    The address tree does not include user circles, for reasons of size and
+    speed (MongoDB limits documents to 16MB).
+    """
+
+    def __init__(self):
+        """Initialize the address tree with a storage interface."""
+        self.storage = get_drum()
+        self.tree: CircleTree = {}
+        self._load_tree()
+
+    def _load_tree(self):
+        """Load the address tree from the database."""
+        # TODO: Recursive conversion of PK from _id
+        self.tree = get_tree_root()
