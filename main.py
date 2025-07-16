8bc45693257328c5806fa75e033297cfc0bdf6d7
nyjc.computing <164971954+nycomp@users.noreply.github.com>
2025-07-16 04:56:41 +0000

    feat: bind localhost for testing and development environments

--- main.py
+++ main.py
@@ -27,4 +27,9 @@ def home():
 
 
 if __name__ == '__main__':
-    app.run('127.0.0.1', port=5000)
+    from common import devops
+    match devops.ENV:
+        case devops.PRODUCTION | devops.STAGING:
+            app.run('0.0.0.0', port=5000)
+        case devops.DEVELOPMENT | devops.TESTING:
+            app.run('127.0.0.1', port=5000)
