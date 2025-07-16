81a79693f3dbdd1ef2faca7301e02365739d417b
JS Ng <ngjunsiang@gmail.com>
2025-07-16 00:30:44 +0000

    refactor: remove common.drum module

--- apps/api/__init__.py
+++ apps/api/__init__.py
@@ -28,13 +28,14 @@ __all__ = [
 
 def create_app() -> Flask:
     """Factory function to create the api app.
-    
+
     This is called if api is run as a standalone app.
     """
     app = Flask(__name__)
     init_app(app)
     errors.init_app(app)
     return app
+
 
 def init_app(app: Flask | Blueprint) -> None:
     """Initialise the API blueprint with the given Flask app."""
@@ -47,10 +48,11 @@ def init_app(app: Flask | Blueprint) -> None:
     routes.emailotp.init_app(bp)
     app.register_blueprint(bp)
 
+
 @devops.block_env(devops.PRODUCTION)
 def init_db() -> None:
     """Initialise the tables needed by api.
-    
+
     This convenience function makes it easier to initialise tables for all
     models.
     """
@@ -61,28 +63,27 @@ def init_db() -> None:
 
     for model in (emailotp, user):
         model.init_db()
-    
+
     # Initialize vault client database
     vault_client.init_db()
 
+
+@devops.block_env(devops.PRODUCTION)
 def purge() -> None:
     """Purge the database.
-    
+
     This function is intended to be used in a test environment to reset the
     database state.
     """
     # These imports do not appear at the top of the file to avoid namespace
     # pollution, as they are typically not used in production.
-    from warnings import warn
+    from warnings import warn  # type: ignore[import-untyped]
+    from storage import purge_all  # type: ignore[import-untyped]
 
-    from common import devops
-
-    if devops.ENV in (devops.STAGING, devops.PRODUCTION):
+    if devops.ENV == devops.STAGING:
         warn(f"Purging database in {devops.ENV} environment.", stacklevel=2)
         if input("Are you sure? (y/n): ").lower() == 'y':
             # User confirmed the purge
-            from common.drum.postgres import purge
-            purge()
+            purge_all()
     else:
-        from common.drum.sqlite import purge
-        purge()
+        purge_all()
