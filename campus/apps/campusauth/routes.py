197e0d762990422a9084333d87a0fa83a8527e5c
JS Ng <ngjunsiang@gmail.com>
2025-08-01 11:28:50 +0800

    docs: add docstrings for oauth endpoints

--- campus/apps/campusauth/routes.py
+++ campus/apps/campusauth/routes.py
@@ -23,20 +23,50 @@ def init_app(app: Flask | Blueprint) -> None:
 def not_implemented():
     return jsonify({"message": "Not implemented"}), 404
 
-@bp.route('/authorize', methods=['POST'])
+@bp.get('/authorize')
 def authorize():
+    """
+    Authorization endpoint for OAuth2.
+
+    This ednpoint:
+    - Validates the user's session or identity.
+    - Presents a consent screen to the user (if necessary).
+    - Issues an authorization code (for authorization code flow).
+    - Redirects the user back to the client with the code.
+    """
     return not_implemented()
 
-@bp.route('/token', methods=['POST'])
+@bp.post('/token')
 def token():
+    """Token endpoint for OAuth2.
+    
+    Used by the client to exchange:
+    - An authorization code for an access token (and optionally a refresh token).
+    - A refresh token for a new access token.
+    - Credentials (in password or client credentials grant types) for tokens.
+    """
     return not_implemented()
 
-@bp.route('/login', methods=['POST'])
+@bp.post('/login')
 def login():
+    """Login endpoint for user authentication.
+
+    This endpoint is used to:
+    - check if an authorised session exists,
+    - redirect the user to authenticate (through Google Workspace) if not,
+    - handle the callback from the authentication provider.
+    - redirect to the authorization endpoint if the user is authenticated.
+    """
     return not_implemented()
 
-@bp.route('/logout', methods=['POST'])
+@bp.post('/logout')
 def logout():
+    """Logout endpoint for user authentication.
+    
+    This endpoint is used to:
+    - Invalidate the user's session.
+    - Optionally redirect the user to a logout confirmation or login page.
+    """
     return not_implemented()
 
 # @bp.post('/authorize')  # OAuth2 authorization endpoint for user consent and code grant
