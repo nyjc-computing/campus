6994704a759bee7c6d0e8e166c09e777e77e0042
JS Ng <ngjunsiang@gmail.com>
2025-08-25 08:59:16 +0000

    refacotr: schema edits after discussion

--- campus/models/event.py
+++ campus/models/event.py
@@ -21,22 +21,24 @@ from campus.models.base import BaseRecord
 # Create a new EventID with CampusID(uid.generate_category_uid("event", length=8))
 EventID = CampusID
 
-### Database-related code
+# Database-related code
 
 """
 Database schema:
 id: TEXT - EventID, is the primary key
+created_at: TEXT - from BaseRecord
 name: TEXT - name of the event
-venue: TEXT - venue of the event
-event_time: TIMESTAMPTZ - time that event starts.
-length: INTEGER - length of event in seconds
-
-created_at: TIMESTAMPTZ - from BaseRecord
+description: TEXT  longer description or details of event
+location: TEXT - location of the event
+location_url: TEXT - URL for the location of the event
+start_time: TEXT - time that event starts.
+duration: INTEGER - duration of event in seconds
 """
 
-# TODO: Implement some venue validation through some master list?
+# TODO: Implement some location validation through some master list?
 
 TABLE = "events"
+
 
 @devops.block_env(devops.PRODUCTION)
 def init_db():
@@ -49,53 +51,62 @@ def init_db():
     storage = get_table(TABLE)
     schema = f"""
         CREATE TABLE IF NOT EXISTS "{TABLE}" (
-            id TEXT PRIMARY KEY NOT NULL,
-            name TEXT NOT NULL,
-            venue TEXT NOT NULL,
-            event_time TIMESTAMPTZ NOT NULL,
-            length INTEGER NOT NULL,
-            created_at TIMESTAMPTZ NOT NULL
+            id TEXT PRIMARY KEY,
+            created_at TEXT NOT NULL,
+            name TEXT,
+            location TEXT,
+            start_time TEXT,
+            duration INTEGER
         )
     """
     storage.init_table(schema)
 
-class EventRecord(BaseRecord, total = True):
+
+class EventRecord(BaseRecord, total=True):
     """The event record stored in the events table."""
     id: EventID
     name: str
-    venue: str
-    event_time: utc_time.datetime
-    length: int # time in seconds
+    location: str
+    location_url: str
+    start_time: str  # rfc3339 string
+    duration: int  # time in minutes
     # Also has created_at from BaseRecord.
 
-### Request body schemas
+# Request body schemas
 
 # NOTE: THIS MODEL DEALS ONLY WITH DATETIMES. THE FLASK FILE WILL DEAL WITH CONVERSIONS FROM STRING.
+
 
 class RequestEventInfo(TypedDict):
     """Request body schema for a request with event info as parameters."""
     name: str
-    venue: str
-    event_time: utc_time.datetime # rfc3339 time if str
-    length: int # time in seconds
+    location: str
+    location_url: str
+    start_time: str  # rfc3339 string
+    duration: int  # time in minutes
 
-class EventNew(RequestEventInfo, total=True): 
+
+class EventNew(RequestEventInfo, total=True):
     """Request body schema for a events.new operation."""
     pass
 
 # total = False as all params are optional.
+
+
 class EventUpdate(RequestEventInfo, total=False):
     """Request body schema for a events.update operation."""
     pass
 
 # events.delete and events.get do not need a request body schema as it takes no params.
 
-### Response body schemas
+# Response body schemas
+
 
 # Response body schema representing the result of a events.get, events.update and events.new operation.
 EventResource = EventRecord
 
-### Model classes.
+# Model classes.
+
 
 class Event:
     """Event model for handling database operations related to events."""
@@ -103,19 +114,20 @@ class Event:
     def __init__(self):
         """Initialize the User model with a table storage interface."""
         self.storage = get_table(TABLE)
-    
+
     def new(self, **fields: Unpack[EventNew]) -> EventResource:
         """Create a new event."""
-        event_id = EventID(uid.generate_category_uid("event", length=8))
+        event_id = EventID(uid.generate_category_uid("event", length=16))
 
         # Least ugly solution due to type gymnastics.
         record = EventRecord(
             id=event_id,
-            created_at=utc_time.now(),
+            created_at=utc_time.to_rfc3339(utc_time.now()),
             name=fields["name"],
-            venue=fields["venue"],
-            length=fields["length"],
-            event_time=fields["event_time"]
+            location=fields["location"],
+            location_url=fields["location_url"],
+            duration=fields["duration"],
+            start_time=utc_time.to_rfc3339(fields["start_time"])
         )
 
         try:
@@ -123,7 +135,7 @@ class Event:
             return EventResource(**record)
         except Exception as e:
             raise api_errors.InternalError(message=str(e), error=e)
-    
+
     def _try_get(self, event_id: EventID) -> EventRecord:
         """Tries to get event by event_id
         Raises InternalError upon other errors."""
@@ -134,31 +146,31 @@ class Event:
                     message="Event not found",
                     event_id=event_id
                 )
-            event = EventRecord(**event) # Assert-coerce to EventResource.
+            event = EventRecord(**event)  # Assert-coerce to EventResource.
         except api_errors.ConflictError:
             raise
         except Exception as e:
             raise api_errors.InternalError(message=str(e), error=e)
-        
+
         return event
-    
+
     def delete(self, event_id: EventID) -> None:
         """Delete an event by id."""
-        self._try_get(event_id) # Make sure it exists.
+        self._try_get(event_id)  # Make sure it exists.
         try:
             self.storage.delete_by_id(event_id)
         except Exception as e:
             raise api_errors.InternalError(message=str(e), error=e)
-    
+
     def get(self, event_id: EventID) -> EventResource:
         """Get an event by id."""
         return self._try_get(event_id)
-    
+
     def update(self, event_id: EventID, **updates: Unpack[EventUpdate]) -> None:
         """Update an event by id."""
         # Check if user exists first
 
-        self._try_get(event_id) # Make sure it exists.
+        self._try_get(event_id)  # Make sure it exists.
         try:
             self.storage.update_by_id(event_id, dict(updates))
         except Exception as e:
