fcc4e5e6fde06cbd640d98ff5946cb821045e312
JS Ng <ngjunsiang@gmail.com>
2025-08-27 03:45:50 +0000

    feat: add client_factory() for campus.client

--- campus/client/__init__.py
+++ campus/client/__init__.py
@@ -5,12 +5,32 @@ Campus Client Package
 Provides unified Campus client interface.
 """
 
+from flask import Flask
+
+from campus.client.config import get_app_base_url
 from campus.client.core import Campus
 from campus.client.errors import (
     CampusClientError,
     AuthenticationError,
     NetworkError
 )
+from campus.client.wrapper import ClientFactory, RequestsClient
+
+
+def client_factory(app: Flask) -> ClientFactory:
+    """Create a client factory for the given Flask app.
+
+    Args:
+        app: Flask application instance
+
+    Returns:
+        ClientFactory: A factory for creating test clients
+    """
+
+    def wrapped_client_factory() -> RequestsClient:
+        return RequestsClient(get_app_base_url(app))
+    return wrapped_client_factory
+
 
 __all__ = [
     'Campus',
