529030d048f7d6905c25cb145e88a3e049ff0a81
JS Ng <ngjunsiang@gmail.com>
2025-08-27 07:45:36 +0000

    format: import config module instead of functions

--- campus/client/vault/__init__.py
+++ campus/client/vault/__init__.py
@@ -3,7 +3,7 @@
 Provides clean module interfaces for Campus Vault service resources.
 """
 
-from campus.config import get_app_base_url
+from campus import config
 from campus.client.vault.vault import VaultResource
 from campus.client.vault.access import VaultAccessResource
 from campus.client.vault.client import VaultClientResource
@@ -12,7 +12,7 @@ from campus.common.http import get_client
 
 def get_vault() -> VaultResource:
     """Get the Vault service client."""
-    vault_base_url = get_app_base_url("campus.vault")
+    vault_base_url = config.get_app_base_url("campus.vault")
     return VaultResource(get_client(base_url=vault_base_url))
 
 
