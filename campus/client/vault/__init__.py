510f637101b311dba096c207cfea81265416c5b4
JS Ng <ngjunsiang@gmail.com>
2025-08-27 04:20:51 +0000

    feat: add get_vault() function to retrieve Vault service client

--- campus/client/vault/__init__.py
+++ campus/client/vault/__init__.py
@@ -6,5 +6,13 @@ Provides clean module interfaces for Campus Vault service resources.
 from campus.client.vault import vault
 from campus.client.vault import access
 from campus.client.vault import client
+from campus.client.vault.vault import VaultResource
+from campus.client.wrapper import ClientFactory
 
-__all__ = ['vault', 'access', 'client']
+
+def get_vault(client_factory: ClientFactory) -> VaultResource:
+    """Get the Vault service client."""
+    return VaultResource(client_factory(), "vault")
+
+
+__all__ = ['vault', 'access', 'client', 'VaultResource', 'get_vault']
