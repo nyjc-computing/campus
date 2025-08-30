97081434278aeb4c93be2e07bde62884c08919ae
Saltensity <matthewna25@gmail.com>
2025-08-30 07:13:58 +0000

    Testing: Addition of "test_yapper.py" to test PostgreSQLYapper

---
+++ tests/test_yapper.py
@@ -0,0 +1,26 @@
+import unittest
+import os
+
+from tests.fixtures.setup import set_test_env_vars
+from tests.fixtures.vault import init_vault, give_vault_access
+from tests.fixtures.yapper import init_yapper
+
+class TestYapper(unittest.TestCase):
+    
+    def test_setup_vars(self):
+        set_test_env_vars()
+        self.assertIsNotNone(os.environ["VAULTDB_URI"])
+    
+    def test_vault_vars_and_access(self):
+        init_vault()
+        self.assertIsNotNone(os.environ["CLIENT_SECRET"])
+        self.assertIsNotNone(os.environ["CLIENT_ID"])
+        
+        give_vault_access()
+    
+    def test_yapper_vars(self):
+        init_yapper()
+        self.assertIsNotNone(os.environ["YAPPERDB_URI"])
+
+if __name__ == "__main__":
+    unittest.main()
