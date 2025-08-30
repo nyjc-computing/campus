5ab522860cc880097df622d9e42a884af9536ea1
JS Ng <ngjunsiang@gmail.com>
2025-08-30 00:37:29 +0000

    fix: remember original branch and switch back to it in background-pull script

--- scripts/background-pull.sh
+++ scripts/background-pull.sh
@@ -13,7 +13,10 @@ if ! git show-ref --verify --quiet "refs/heads/$BRANCH"; then
     exit 2
 fi
 
+# Remember the current branch
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
 git stash
 git checkout "$BRANCH" && git pull
-git checkout deployment-refactor
+git checkout "$CURRENT_BRANCH"
 git stash pop
