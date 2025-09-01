a44c4a021594867c23d6eded4c9e36031883145c
JS Ng <ngjunsiang@gmail.com>
2025-09-01 00:25:00 +0000

    feat: export main client classes

--- campus/client/apps/__init__.py
+++ campus/client/apps/__init__.py
@@ -4,9 +4,16 @@ Provides clean module interfaces for Campus Apps service resources.
 """
 
 from . import admin, circles, users
+from .admin import AdminClient
+from .circles import CirclesClient
+from .users import UsersClient
+
 
 __all__ = [
     'admin',
     'circles',
     'users',
+    'AdminClient',
+    'CirclesClient',
+    'UsersClient',
 ]
--- campus/client/vault/__init__.py
+++ campus/client/vault/__init__.py
@@ -4,9 +4,11 @@ Provides clean module interfaces for Campus Vault service resources.
 """
 
 from . import access, client, vault
+from .vault import VaultClient
 
 __all__ = [
     'access',
     'client',
     'vault',
+    'VaultClient',
 ]
