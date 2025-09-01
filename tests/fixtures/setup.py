c9d44a8f8eec7ed6f0ee340a4b84432797ea6d9d
JS Ng <ngjunsiang@gmail.com>
2025-09-01 05:14:07 +0000

    refactor: enhance test environment setup by adding CLIENT_ID and CLIENT_SECRET variables

--- tests/fixtures/setup.py
+++ tests/fixtures/setup.py
@@ -8,5 +8,8 @@ import os
 def set_test_env_vars():
     """Set environment variables for testing."""
     os.environ["ENV"] = "testing"
+    os.environ["CLIENT_ID"] = "test_client"
+    os.environ["CLIENT_SECRET"] = "test_secret"
+
+def set_vault_env_vars():
     os.environ["VAULTDB_URI"] = "postgresql://vaultuser:vaultpass@db_vault:5432/vaultdb"
-    # Other secrets will be stored inside vault
