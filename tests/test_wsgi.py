21ed7eeda7f5bc343b8a0df40f0e5a67112c1ce3
SavioNYJC <savio.sidharta_2025@nyjc.edu.sg>
2025-09-01 13:47:48 +0000

    updated to include vault as a testcase as well

--- tests/test_wsgi.py
+++ tests/test_wsgi.py
@@ -1,10 +1,15 @@
-import wsgi
 import unittest
 import os
 
 class TestWSGI(unittest.TestCase):
     def test_wsgi_import(self):
         os.environ["DEPLOY"] = "apps"
+        import wsgi 
+        from wsgi import app
+        self.assertIsNotNone(app, "App should not be None")
+
+        os.environ["DEPLOY"] = "vault"
+        import wsgi 
         from wsgi import app
         self.assertIsNotNone(app, "App should not be None")
 
