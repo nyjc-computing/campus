107ceaf5c1d972b4cbea3ec5e1162dae41f72a1d
JS Ng <ngjunsiang@gmail.com>
2025-09-02 08:55:51 +0800

    refactor: eliminate code duplication

--- tests/test_wsgi.py
+++ tests/test_wsgi.py
@@ -3,15 +3,12 @@ import os
 
 class TestWSGI(unittest.TestCase):
     def test_wsgi_import(self):
-        os.environ["DEPLOY"] = "apps"
-        import wsgi 
-        from wsgi import app
-        self.assertIsNotNone(app, "App should not be None")
+        for deploy_mode in ("apps", "vault"):
+            os.environ["DEPLOY"] = deploy_mode
+            import wsgi 
+            from wsgi import app
+            self.assertIsNotNone(app, "App should not be None")
 
-        os.environ["DEPLOY"] = "vault"
-        import wsgi 
-        from wsgi import app
-        self.assertIsNotNone(app, "App should not be None")
 
 if __name__ == "__main__":
     unittest.main()
