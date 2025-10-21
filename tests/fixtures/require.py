9e9718652011e3fe18354c5aa02a866325b0b4f4
JS Ng <ngjunsiang@gmail.com>
2025-10-21 06:56:43 +0000

    format: fix whitespacing

--- tests/fixtures/require.py
+++ tests/fixtures/require.py
@@ -5,10 +5,12 @@ Functions for asserting the existence of test prerequisites.
 
 import campus.common.env as campus_env
 
+
 def env(env: str) -> None:
     """Raise a RuntimeError if not in the required environment."""
     if (this_env := campus_env.ENV) != env:
         raise RuntimeError(f"ENV != {env} (currently in {this_env})")
+
 
 def envvar(var: str) -> str:
     """Raise an EnvironmentError if the required environment variable
