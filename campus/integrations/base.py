da25f5015c95cd4128ced962ae0fbb0308ab27b3
JS Ng <ngjunsiang@gmail.com>
2025-10-30 06:50:38 +0000

    feat: add Provider interface

--- campus/integrations/base.py
+++ campus/integrations/base.py
@@ -4,10 +4,16 @@ Schema to describe the JSON files describing third-party integration
 configurations.
 """
 
-from typing import Literal, NotRequired, TypedDict
+from abc import ABC, abstractmethod
+import contextlib
+from typing import Any, Iterator, Literal, NotRequired, Self, TypedDict
+
+import flask
+import werkzeug
 
 from campus.common import schema
 from campus.common.devops import Env
+from campus.models import token, webauth
 
 HttpScheme = Literal["basic", "bearer"]
 OAuth2Flow = Literal[
@@ -22,6 +28,8 @@ Security = Literal[
     "oauth2",
     "openIdConnect"
 ]
+
+tokens = token.Tokens()
 
 
 class SecurityConfigSchema(TypedDict):
@@ -62,3 +70,53 @@ class IntegrationConfigSchema(TypedDict):
     api_doc: schema.Url
     discovery_url: schema.Url
     security: dict[Security, SecurityConfigSchema]
+
+
+class Provider(ABC):
+    """Base provider class for OAuth2 integrations.
+
+    This class encapsulates the metadata and core OAuth2 operations
+    for a third-party authentication provider.
+    """
+    provider: str
+    title: str
+    description: str
+    version: str
+    openapi_version: str
+    authorization_url: schema.Url
+    token_url: schema.Url
+    security_scheme: webauth.SecurityScheme
+    _headers: dict[str, str]
+    _token: token.TokenRecord | None = None
+    _CLIENT_ID: str
+    _CLIENT_SECRET: str
+
+    def with_token(self, token: token.TokenRecord) -> Self:
+        """A chainable method for passing a token to the instance."""
+        self._token = token
+        return self
+
+    def release_token(self) -> token.TokenRecord | None:
+        """Release the token held by the provider, if any."""
+        self._token = None
+
+    @abstractmethod
+    def redirect_for_authorization(
+            self,
+            target: schema.Url,
+    ) -> werkzeug.Response:
+        """Return a 302 Redirect response to the provider's
+        authorization URL.
+        """
+
+    @contextlib.contextmanager
+    def authorize_for_user(
+            self: Self,
+            user_id: schema.UserID
+    ) -> Iterator[Self]:
+        """Context manager to authorize a user API use."""
+        token = tokens.get_by_client_user(self._CLIENT_ID, user_id)
+        try:
+            yield self.with_token(token)
+        finally:
+            self.release_token()
