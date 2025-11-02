f57d89ed2f6c2c3b6a36679cec7a6308ef44ee42
JS Ng <ngjunsiang@gmail.com>
2025-11-02 08:04:02 +0000

    feat: add campus.common.utils.datacls module
    This module provides helper functions for working with Python dataclasses

---
+++ campus/common/utils/datacls.py
@@ -0,0 +1,44 @@
+"""campus.common.utils.datacls
+
+Utilities for working with Python dataclasses and type signatures.
+"""
+
+import dataclasses
+import typing
+
+
+def get_pytype(t: type) -> type:
+    """Get the underlying Python type of a dataclass field, stripping
+    Optional if present.
+    """
+    origin = typing.get_origin(t)
+    if origin is typing.Union:
+        args = typing.get_args(t)
+        non_none_args = [arg for arg in args if arg is not type(None)]
+        if len(non_none_args) == 1:
+            return non_none_args[0]
+    return t
+
+
+def has_default(field: dataclasses.Field) -> bool:
+    """Check if a dataclass field has a default value."""
+    return field.default is not dataclasses.MISSING
+
+
+def is_optional(field: dataclasses.Field) -> bool:
+    """Check if a dataclass field is Optional.
+
+    A field is optional if:
+    - field.type annotation is of the form Optional[T] or Union[T, None]
+    - It has a field.default value
+    """
+    t = field.type
+    origin = typing.get_origin(t)
+    if not origin is typing.Union:
+        return False
+    args = typing.get_args(t)
+    if not type(None) in args:
+        return False
+    if not has_default(field):
+        return False
+    return True
