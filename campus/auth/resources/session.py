cb389cbad22cbd439710b899223eddd8f912fd33
JS Ng <ngjunsiang@gmail.com>
2025-11-05 01:28:34 +0000

    refactor: remove client-side session management

--- campus/auth/resources/session.py
+++ campus/auth/resources/session.py
@@ -1,11 +1,12 @@
 """campus.auth.resources.session
 
 Auth session resource for Campus API.
+
+Client-side session management is handled by first-/third-party apps,
+e.g. through campus-api-python
 """
 
 import typing
-
-import flask
 
 from campus.common import schema
 from campus.common.errors import api_errors, auth_errors
@@ -14,6 +15,12 @@ import campus.model
 import campus.storage
 
 session_storage = campus.storage.get_collection("auth_sessions")
+
+
+def init_storage() -> None:
+    """Initialize storage for client authentication."""
+    # Document store does not require initialization
+    pass
 
 
 class AuthSessionsResource:
@@ -112,33 +119,26 @@ class ProviderAuthSessionResource:
         state: str | None = None,
         target: schema.Url | None = None,
     ) -> campus.model.AuthSession:
-        """Create a new session.
-
-        Any existing session will be revoked.
-        """
-        # Delete any existing session
-        if (existing_session_id := _check_existing_id(self.provider)):
-            self[existing_session_id].delete()
+        """Create a new session."""
         session = _from_record({
             "id": uid.generate_category_uid(f"{self.provider}-session"),
             "expiry_seconds": expiry_seconds,
-            "client_id": str(client_id),
-            "user_id": str(user_id) if user_id else None,
-            "redirect_uri": str(redirect_uri),
+            "client_id": client_id,
+            "user_id": user_id,
+            "redirect_uri": redirect_uri,
             "scopes": scopes or [],
             "authorization_code": (
                 authorization_code
                 or secret.generate_authorization_code()
             ),
             "state": state,
-            "target": str(target) if target else None,
+            "target": target,
         })
         try:
             session_storage.insert_one(session.to_storage())
         except Exception as e:
             raise api_errors.InternalError.from_exception(e)
         else:
-            flask.session[_session_key(self.provider)] = session.id
             return session
 
 
@@ -153,34 +153,17 @@ class AuthSessionResource:
         self.parent = parent
         self.session_id = session_id
 
-    def delete(self, sync_client: bool = True) -> None:
+    def delete(self) -> None:
         """Delete this auth session."""
         provider = self.parent.provider
-        # Check if session exists client-side
-        session_id = (
-            self.session_id or _check_existing_id(provider)
-        )
-        if not session_id:
-            raise api_errors.NotFoundError(
-                message="Session not found",
-                session_id=session_id
-            ) from None
-        if sync_client:
-            session_id = _verify_session_id(provider, session_id)
-        # Remove server-side session
         try:
-            session_storage.delete_by_id(session_id)
+            session_storage.delete_by_id(self.session_id)
         except campus.storage.errors.NotFoundError:
             # Session already deleted server-side
             # TODO: logging for missing session
             pass
         except Exception as e:
             raise api_errors.InternalError.from_exception(e)
-        else:
-            # For consistency, only remove client-side session after
-            # successful server-side deletion
-            if sync_client and _session_key(provider) in flask.session:
-                del flask.session[_session_key(provider)]
 
     def finalize(self) -> schema.Url | None:
         """Finalize an auth session and return the redirect URI.
@@ -189,7 +172,7 @@ class AuthSessionResource:
         """
         auth_session = self.get()
         self.delete()
-        return auth_session.redirect_uri
+        return auth_session.target
 
     def get(self) -> campus.model.AuthSession:
         """Get the auth session record.
@@ -197,45 +180,31 @@ class AuthSessionResource:
         Returns:
             AuthSession instance
         """
-        session_id = self.session_id
-        record = session_storage.get_by_id(session_id)
+        record = session_storage.get_by_id(self.session_id)
         if not record:
             raise api_errors.NotFoundError(
-                f"Session '{session_id}' not found",
-                session_id=session_id
+                f"Session '{self.session_id}' not found",
+                session_id=self.session_id
             )
         return _from_record(record)
 
-    def update(self, **update) -> campus.model.AuthSession:
-        """Update an existing session."""
+    def update(
+            self,
+            session_id: schema.CampusID,
+            *,
+            user_id: schema.UserID
+    ) -> campus.model.AuthSession:
+        """Update an existing session.
+        
+        Only user_id can be updated, since it will only be known after
+        user has authenticated.
+        """
         provider = self.parent.provider
-        session_id = self.session_id or _check_existing_id(provider)
-        if not session_id:
-            raise api_errors.NotFoundError(
-                message="Session not found",
-                session_id=session_id
-            ) from None
-        _verify_session_id(provider, session_id)
-        # TODO: Validate update fields
-        for immutable_key in (
-            schema.CAMPUS_KEY,
-            "client_id",
-            "user_id",
-            "agent_string",
-            "created_at",
-            "expires_at",
-            "provider",
-            "redirect_uri",
-            "state",
-            "target",
-            "scopes",
-        ):
-            if immutable_key in update:
-                raise api_errors.InvalidRequestError(
-                    message=f"Cannot update immutable field: {immutable_key}"
-                )
         try:
-            session_storage.update_by_id(session_id, update)
+            session_storage.update_by_id(
+                session_id,
+                {"user_id": str(user_id)}
+            )
         except campus.storage.errors.NotFoundError as e:
             raise api_errors.ConflictError(
                 message="Session not found",
@@ -270,68 +239,3 @@ def _from_record(
     if "target" in record and record["target"] is not None:
         args["target"] = schema.Url(record["target"])
     return campus.model.AuthSession(**args)
-
-
-def _session_key(provider: str) -> str:
-    """Get the session key for the provider."""
-    return f"{provider}_session_id"
-
-
-def init_storage() -> None:
-    """Initialize storage for client authentication."""
-    # Document store does not require initialization
-    pass
-
-
-def _check_existing_id(provider: str) -> schema.CampusID | None:
-    """Check if a session already exists.
-
-    A session exists if:
-    - there is a session_id stored in the client cookie, and
-    - a session with that ID exists on the server.
-    Returns the session ID if a session exists, otherwise None.
-    If the session ID is found client-side but not server-side, the
-    client-side session is considered invalid and revoked; this does
-    not trigger an error as it is expected to happen when
-    server-side sweeps happen.
-    """
-    session_key = _session_key(provider)
-    client_session_id = flask.session.get(session_key)
-    if not client_session_id:
-        return None
-    try:
-        session_storage.get_by_id(client_session_id)
-    except campus.storage.errors.NotFoundError:
-        # Revoke client-side
-        flask.session.pop(session_key, None)
-        return None
-    except Exception as e:
-        raise api_errors.InternalError.from_exception(e)
-    else:
-        return client_session_id
-
-
-def _verify_session_id(
-        provider: str,
-        session_id: schema.CampusID
-) -> schema.CampusID:
-    """Verify the session ID against the stored session.
-    Raises:
-        ConflictError if there is a mismatch.
-        NotFoundError if no session ID is found in the client.
-
-    This avoids accidental deletion of a session that does not
-    belong to the client.
-    """
-    client_session_id = _check_existing_id(provider)
-    if client_session_id and session_id != client_session_id:
-        raise api_errors.ConflictError(
-            message="Mismatch with client session ID",
-            session_id=session_id
-        ) from None
-    if not client_session_id:
-        raise api_errors.NotFoundError(
-            message="No session ID in client",
-            session_id=session_id
-        ) from None
-    return client_session_id
