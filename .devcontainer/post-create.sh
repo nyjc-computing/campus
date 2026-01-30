262674408a248f10944427881edc03d6e61bd8dd
JS Ng <ngjunsiang@gmail.com>
2026-01-30 02:29:39 +0000

    devops: build gh and poetry into devcontainer image
    - Create custom Dockerfile that installs gh and poetry during build
    - Update docker-compose.yml to use custom Dockerfile
    - Simplify post-create.sh to only handle git config and poetry install
    - Ensures gh and poetry are always available in scripts
    - Faster startup - no need to wait for tool installation

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -6,12 +6,8 @@ git config pull.ff true
 # Set up pre-push hook to catch sanity check failures early
 git config core.hooksPath .githooks
 
-# Install Poetry and project dependencies
-pip install poetry
+# Install project dependencies (gh and poetry already in image)
 poetry install --no-root  # don't install campus package
-
-# Install poetry-shell plugin
-poetry self add poetry-plugin-shell
 
 # Activate poetry venv (for pylance auto-import)
 poetry shell
